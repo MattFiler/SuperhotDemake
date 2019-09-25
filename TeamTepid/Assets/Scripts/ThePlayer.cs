47112d532175f6ea110031c6e1f5c95ba4622f59
MattFiler <matthew.filer@hotmail.co.uk>
2019-09-25 18:32:10 +0100

    Improved movement / updated game time calculation

--- TeamTepid/Assets/Scripts/ThePlayer.cs
+++ TeamTepid/Assets/Scripts/ThePlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ThePlayer : MonoBehaviour
 {
-    [SerializeField] private float MaxVelocity = 50000.0f;
+    [SerializeField] private float MaxVelocity = 300.0f;
     [SerializeField] private float VelocityChangeRate = 5000.0f;
     private Rigidbody2D thisRigidbody;
     private RectTransform spriteTransform;
@@ -22,48 +22,48 @@ public class ThePlayer : MonoBehaviour
         //FORWARD
         if (Input.GetKey(KeyCode.W))
         {
-            if (thisRigidbody.velocity.y <= MaxVelocity * 10000)
+            if (thisRigidbody.velocity.y <= MaxVelocity)
             {
-                thisRigidbody.AddForce(new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 10000));
+                thisRigidbody.velocity += new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 100.0f);
             }
             if (thisRigidbody.velocity.x != 0)
             {
-                thisRigidbody.velocity -= new Vector2(Time.fixedDeltaTime * thisRigidbody.velocity.x * 2, 0.0f);
+                thisRigidbody.velocity -= new Vector2(Time.fixedDeltaTime * thisRigidbody.velocity.x * 4, 0.0f);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
         //RIGHT
         if (Input.GetKey(KeyCode.D))
         {
-            if (thisRigidbody.velocity.x <= MaxVelocity * 10000)
+            if (thisRigidbody.velocity.x <= MaxVelocity)
             {
-                thisRigidbody.AddForce(new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 10000, 0.0f));
+                thisRigidbody.velocity += new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 100.0f, 0.0f);
             }
             if (thisRigidbody.velocity.y != 0)
             {
-                thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 2);
+                thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 4);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0,0,270));
         }
         //LEFT
         if (Input.GetKey(KeyCode.A))
         {
-            if (thisRigidbody.velocity.x >= -(MaxVelocity * 10000))
+            if (thisRigidbody.velocity.x >= -MaxVelocity)
             {
-                thisRigidbody.AddForce(-new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 10000, 0.0f));
+                thisRigidbody.velocity -= new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 100.0f, 0.0f);
             }
             if (thisRigidbody.velocity.y != 0)
             {
-                thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 2);
+                thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 4);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
         }
         //BACK
         if (Input.GetKey(KeyCode.S))
         {
-            if (thisRigidbody.velocity.y >= -(MaxVelocity * 10000))
+            if (thisRigidbody.velocity.y >= -MaxVelocity)
             {
-                thisRigidbody.AddForce(-new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 10000));
+                thisRigidbody.velocity -= new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 100.0f);
             }
             if (thisRigidbody.velocity.x != 0)
             {
@@ -71,20 +71,18 @@ public class ThePlayer : MonoBehaviour
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
         }
-
-        /*
+        
         //Change game speed based on velocity
         float velocityX = (thisRigidbody.velocity.x > 0) ? thisRigidbody.velocity.x : thisRigidbody.velocity.x * -1;
         float velocityY = (thisRigidbody.velocity.y > 0) ? thisRigidbody.velocity.y : thisRigidbody.velocity.y * -1;
-        float normalisedVelocityX = velocityX / MaxVelocity;
+        float normalisedVelocityX = velocityX / (MaxVelocity - (MaxVelocity / 5));
         if (normalisedVelocityX > 1.0f) { normalisedVelocityX = 1.0f; }
-        float normalisedVelocityY = velocityY / MaxVelocity;
+        float normalisedVelocityY = velocityY / (MaxVelocity - (MaxVelocity / 5));
         if (normalisedVelocityY > 1.0f) { normalisedVelocityY = 1.0f; }
         float normalisedVelocityBothDirs = normalisedVelocityX + normalisedVelocityY;
         if (normalisedVelocityBothDirs >= 1.0f) { normalisedVelocityBothDirs = 1.0f; }
-        //Debug.Log(normalisedVelocityBothDirs);
+        Debug.Log("GAME TIME SHOULD BE: " + normalisedVelocityBothDirs);
         //Time.timeScale = normalisedVelocityBothDirs;
         //Time.fixedDeltaTime = Time.timeScale;
-        */
     }
 }
