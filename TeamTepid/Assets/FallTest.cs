53f3d60727281a7cb557e0645ed604559958e622
MattFiler <matthew.filer@hotmail.co.uk>
2019-09-25 18:46:34 +0100

    Game time is dependant on player speed

---
+++ TeamTepid/Assets/FallTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallTest : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        gameObject.transform.position += new Vector3(0.0f, 5.0f, 0.0f);
+        if (gameObject.transform.position.y >= 600.0f)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 0.0f, 0.0f);
+        }
+    }
+}
--- TeamTepid/Assets/Scripts/ThePlayer.cs
+++ TeamTepid/Assets/Scripts/ThePlayer.cs
@@ -16,56 +16,56 @@ public class ThePlayer : MonoBehaviour
         spriteTransform = gameObject.transform.GetChild(0).GetComponent<RectTransform>();
     }
     
-    /* Handle player input */
-    void FixedUpdate()
+    /* Handle player input & adjust game speed */
+    void Update()
     {
-        //FORWARD
+        //Player input: forwards
         if (Input.GetKey(KeyCode.W))
         {
             if (thisRigidbody.velocity.y <= MaxVelocity)
             {
                 thisRigidbody.velocity += new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 100.0f);
             }
-            if (thisRigidbody.velocity.x != 0)
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && thisRigidbody.velocity.x != 0)
             {
                 thisRigidbody.velocity -= new Vector2(Time.fixedDeltaTime * thisRigidbody.velocity.x * 4, 0.0f);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
         }
-        //RIGHT
+        //Player input: right
         if (Input.GetKey(KeyCode.D))
         {
             if (thisRigidbody.velocity.x <= MaxVelocity)
             {
                 thisRigidbody.velocity += new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 100.0f, 0.0f);
             }
-            if (thisRigidbody.velocity.y != 0)
+            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && thisRigidbody.velocity.y != 0)
             {
                 thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 4);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0,0,270));
         }
-        //LEFT
+        //Player input: left
         if (Input.GetKey(KeyCode.A))
         {
             if (thisRigidbody.velocity.x >= -MaxVelocity)
             {
                 thisRigidbody.velocity -= new Vector2(VelocityChangeRate * Time.fixedDeltaTime * 100.0f, 0.0f);
             }
-            if (thisRigidbody.velocity.y != 0)
+            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && thisRigidbody.velocity.y != 0)
             {
                 thisRigidbody.velocity -= new Vector2(0.0f, Time.fixedDeltaTime * thisRigidbody.velocity.y * 4);
             }
             spriteTransform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
         }
-        //BACK
+        //Player input: back
         if (Input.GetKey(KeyCode.S))
         {
             if (thisRigidbody.velocity.y >= -MaxVelocity)
             {
                 thisRigidbody.velocity -= new Vector2(0.0f, VelocityChangeRate * Time.fixedDeltaTime * 100.0f);
             }
-            if (thisRigidbody.velocity.x != 0)
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && thisRigidbody.velocity.x != 0)
             {
                 thisRigidbody.velocity -= new Vector2(Time.fixedDeltaTime * thisRigidbody.velocity.x * 2, 0.0f);
             }
@@ -81,8 +81,8 @@ public class ThePlayer : MonoBehaviour
         if (normalisedVelocityY > 1.0f) { normalisedVelocityY = 1.0f; }
         float normalisedVelocityBothDirs = normalisedVelocityX + normalisedVelocityY;
         if (normalisedVelocityBothDirs >= 1.0f) { normalisedVelocityBothDirs = 1.0f; }
-        Debug.Log("GAME TIME SHOULD BE: " + normalisedVelocityBothDirs);
-        //Time.timeScale = normalisedVelocityBothDirs;
+        Debug.Log("GAME TIME IS: " + normalisedVelocityBothDirs);
+        Time.timeScale = normalisedVelocityBothDirs;
         //Time.fixedDeltaTime = Time.timeScale;
     }
 }
