2cb779738432c64e6f1f69991efd0d798d25ec64
Evan Diamond <evan2.diamond@live.uwe.ac.uk>
2019-09-27 12:01:36 +0100

    Moved the prop pick up and throw to the player
    Moved the prop pick up and throw to the player to allow for directional throws

---
+++ TeamTepid/Assets/Scripts/PropInteraction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropInteraction : MonoBehaviour
+{
+    [Tooltip("The delay between rotaion steps - used to make rotaion seem janky")]
+    [SerializeField] public float rotationDelay = 0.5f;
+    [Tooltip("The amount of rotation per step")]
+    [SerializeField] public float rotationStep = 5;
+    public Vector2 defaultThrowDirection = Vector2.right;
+
+    private bool propThrown = false;
+    private bool startSpin = false;
+    private bool destroyed = false;
+
+    //private Transform playerTransform;
+    //private bool startThrow = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (startSpin)
+        {
+            StartCoroutine(SpinProp());
+            startSpin = false;
+        }
+
+    }
+
+    //public void ThrowProp(Vector2 throwDirection)
+    //{
+    //    
+    //    transform.Rotate(new Vector3(0, 0, rotationStep));
+    //    StartCoroutine(SpinProp());
+    //    startThrow = false;
+    //}
+
+    IEnumerator SpinProp()
+    {
+        while (!destroyed)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + rotationStep);
+            yield return new WaitForSeconds(rotationDelay);
+        }
+    }
+
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        playerTransform = collision.transform;
+    //    }
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        playerTransform = null;
+    //    }
+    //}
+
+    public void PickUpProp(Transform pickUpTransform)
+    {
+        transform.parent = pickUpTransform;
+    }
+
+    public void ThrowProp(Vector2 throwDirection, float throwSpeed)
+    {
+        GetComponent<Rigidbody2D>().velocity = throwDirection.normalized * throwSpeed;
+        propThrown = true;
+        startSpin = true;
+    }
+}
---
+++ TeamTepid/Assets/UseProp.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseProp : MonoBehaviour
+{
+    [SerializeField] public float throwSpeed = 0;
+
+    private bool pickedUpProp;
+    private PropInteraction currentProp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentProp != null)
+        {
+            if(Input.GetKeyDown("e"))
+            {
+                if(!pickedUpProp)
+                {
+                    currentProp.PickUpProp(transform);
+                    pickedUpProp = true;
+                }
+                else
+                {
+                    pickedUpProp = false;
+                    Vector2 currentDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+                    //If player is not moving then throw is set to a default direction
+                    currentProp.ThrowProp(currentDirection == Vector2.zero ? currentProp.defaultThrowDirection : currentDirection, throwSpeed);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Prop"))
+        {
+            currentProp = collision.GetComponent<PropInteraction>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Prop"))
+        {
+            currentProp = null;
+        }
+    }
+}
