5bf0a5aba667368395b3bda4c34f2280701ad5a4
MattFiler <matthew.filer@hotmail.co.uk>
2019-09-27 17:19:50 +0100

    game time manager coming soon (tm)

---
+++ TeamTepid/Assets/TimeManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //THIS WILL DO SOMETHING AT SOME POINT IN THE NEAR FUTURE
+            //I PROMISE
+        }
+    }
+}
