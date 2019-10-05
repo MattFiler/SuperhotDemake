a2f8df70a415472ef14dae16be3d37cbaeb617ae
MattFiler <matthew.filer@hotmail.co.uk>
2019-10-05 18:31:46 +0100

    level loader

---
+++ TeamTepid/Assets/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoSingleton<LevelLoader>
+{
+    [SerializeField] private List<GameObject> LevelPrefabs;
+    private GameObject CurrentLevel;
+
+    /* On start of game, load level 0 */
+    private void Start()
+    {
+        LoadNewLevel(0);
+    }
+
+    /* Load a new level by index */
+    public void LoadNewLevel(int index)
+    {
+        Destroy(CurrentLevel);
+        CurrentLevel = Instantiate(LevelPrefabs[index], new Vector3(0,0,0), Quaternion.identity) as GameObject;
+    }
+
+    /* Return the current level as a GameObject */
+    public GameObject GetCurrentLevel()
+    {
+        return CurrentLevel;
+    }
+}
