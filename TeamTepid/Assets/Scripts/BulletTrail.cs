2d08c9ea37710949f7db01fb1e68f26fe281caba
Toby <Toby>
2019-10-08 13:21:14 +0100

    Bullet trail script (untested)

--- TeamTepid/Assets/Scripts/BulletTrail.cs
+++ TeamTepid/Assets/Scripts/BulletTrail.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class BulletTrail : MonoBehaviour
 {
+    [SerializeField] private float maxTrailLength = 2;
+    LineRenderer lr;
+
+    private float trailLength = 0;
+    private Vector3 addZ = new Vector3(0, 0, 1);
     // Start is called before the first frame update
     void Start()
     {
-        
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, transform.position + addZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        trailLength = Vector3.Distance(transform.position, lr.GetPosition(0));
+        if (trailLength > maxTrailLength)
+        {
+            Vector3 direction = transform.position - lr.GetPosition(0);
+            direction.Normalize();
+            lr.SetPosition(0, (transform.position - direction) + addZ);
+        }
+        lr.SetPosition(1, transform.position + addZ);
+
     }
 }
