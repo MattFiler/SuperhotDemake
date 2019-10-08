5af5c37a4c271ce6f64e4de4d3aead2b5f34b75d
Toby <Toby>
2019-10-08 19:41:59 +0100

    Null ref of player death fix

--- TeamTepid/Assets/Scripts/ItemPickupZone.cs
+++ TeamTepid/Assets/Scripts/ItemPickupZone.cs
@@ -6,12 +6,14 @@ public class ItemPickupZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (transform.parent == null) return;
         Debug.Log("Enter");
         transform.SendMessageUpwards("OnChildTriggerEnter2D", collision);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (transform.parent == null) return;
         transform.SendMessageUpwards("OnChildTriggerExit2D", collision);
     }
 }
